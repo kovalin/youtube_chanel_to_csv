874034468a36aa7f6f81ecfa0547e8780ed7ce9e
Anton Kovalin <9820498@gmail.com>
2018-07-25 15:05:04 +0500

    First commit!

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$API_key    = '{YOU_YOUTUBE_API}';
+$channelID  = 'UC29ju8bIPH5as8OGnQzwJyA';
+$maxResults = 50;
+$maxVideo = 1000;
+$step = 0;
+$nextPageToken = '';
+$output = '';
+
+
+do {
+  $nextPage = ($step > 0) ? '&pageToken='.$nextPageToken : '';
+  $videoList = json_decode(file_get_contents('https://www.googleapis.com/youtube/v3/search?order=date'.$nextPage.'&part=snippet&channelId=' . $channelID . '&maxResults=' . $maxResults . '&key=' . $API_key . ''));
+
+  foreach ($videoList->items as $item) {
+      $output .= $item->snippet->publishedAt . ";" . $item->id->videoId . ";" . $item->snippet->title . ";\r\n";
+  }
+
+  $step += $maxResults;
+  $nextPageToken = $videoList->nextPageToken;
+} while($step<$maxVideo);
+
+file_put_contents('base.csv', $output);
