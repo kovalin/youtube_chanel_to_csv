c556cb3ab2e15de6dad69dc7817a1699b6ea424f
Anton Kovalin <9820498@gmail.com>
2018-07-25 15:11:12 +0500

    Minor fix.

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ do {
   $videoList = json_decode(file_get_contents('https://www.googleapis.com/youtube/v3/search?order=date'.$nextPage.'&part=snippet&channelId=' . $channelID . '&maxResults=' . $maxResults . '&key=' . $API_key . ''));
 
   foreach ($videoList->items as $item) {
-      $output .= $item->snippet->publishedAt . ";" . $item->id->videoId . ";" . $item->snippet->title . ";\r\n";
+      $output .= $item->snippet->publishedAt . ";https://www.youtube.com/watch?v=" . $item->id->videoId . ";" . $item->snippet->title . ";\r\n";
   }
 
   $step += $maxResults;
